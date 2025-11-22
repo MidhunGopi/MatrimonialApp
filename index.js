b7a582c52211ce7e4abdbd4e48592252f3fc7daa
midhu <midhu@ACERMG>
2025-11-22 20:49:32 +0530

    chore: initialize Android project with Gradle wrapper and configuration files
    - Added gradle-wrapper.properties to configure Gradle distribution.
    - Created gradlew and gradlew.bat scripts for executing Gradle tasks.
    - Set up settings.gradle to define project structure and include necessary modules.
    - Initialized app.json for application metadata.
    - Configured Babel with babel.config.js for React Native.
    - Created index.js as the entry point for the application.
    - Set up metro.config.js for Metro bundler configuration.

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './mobile/src/App';
+import {name as appName} from './mobile/app.json';
+
+AppRegistry.registerComponent(appName, () => App);
